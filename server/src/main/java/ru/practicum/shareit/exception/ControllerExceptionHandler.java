a9b6f34737e81c7135fe041c196d95081ebf35fe
aleksey <alexx308@mail.ru>
2024-06-11 18:34:49 +0400

    fix: style

--- server/src/main/java/ru/practicum/shareit/exception/ControllerExceptionHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ControllerExceptionHandler.java
@@ -3,12 +3,9 @@ package ru.practicum.shareit.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import javax.validation.ValidationException;
 
 @Slf4j
 @RestControllerAdvice
